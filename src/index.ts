d886d618cabde0e6abee14ffb2b93150defd9a3b
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-02-02 09:28:10 -0500

    Adding more exports that I forgot.

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@
 export * from "./classes/PayPal.js";
 
 export * from "./types/AddTracking.js";
+export * from "./types/Orders.js";
 export * from "./types/Shared.js";
+export * from "./types/WebhooksManagement.js";
