d21b3bd9b3b662c1ad23569d26e2ee09a300e913
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-02-08 14:59:28 -0500

    Exporting Payments.ts.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.1
+Forgot to export those types, woops.
+
 ## 0.2.0
 Added some Payment related types.
 
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * from "./classes/PayPal.js";
 
 export * from "./types/AddTracking.js";
 export * from "./types/Orders.js";
+export * from "./types/Payments.js";
 export * from "./types/Shared.js";
 export * from "./types/WebhooksManagement.js";
