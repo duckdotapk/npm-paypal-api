da81564744b332ad88e7eb4a8162e503cdba9401
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-01-29 23:22:33 -0500

    Adding some new orders types.

---
+++ src/types/orders/PayPalVaultInstructionBase.ts
@@ -0,0 +1,9 @@
+//
+// Type
+//
+
+/** @see https://developer.paypal.com/docs/api/orders/v2/#definition-vault_instruction_base */
+export interface PayPalVaultInstructionBase
+{
+	store_in_vault? : "ON_SUCCESS";
+}
